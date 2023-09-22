f20c3b16e77b2111cf57343b43cd24e689f662b4
aseel <hamadaseel458@gmail.com>
2023-09-22 07:27:28 -0700

    a

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * rot13 - main function
- *
- * @s: String poiner
- *
- * Return: String function
+* rot13 - main function
+*
+* @s: String poiner
+*
+* Return: String function
 */
 
 char *rot13(char *s)
 {
-
 int k, r;
 
 char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
@@ -22,12 +21,19 @@ for (k = 0; s[k] != '\0'; k++)
 
 for (r = 0; r < 52; r++)
 {
+
 if (s[k] == data1[r])
 {
+
 s[k] = datarot[r];
 
 break;
+
 }
 }
+
 }
+
 return (s);
+
+}
